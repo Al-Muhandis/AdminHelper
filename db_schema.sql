d4ef981c027594cb8eb0322403f78f264aa3fb94
Renat Suleymanov <mail@renat.su>
2024-10-08 02:11:33 +0400

    Create db_schema.sql

---
+++ db_schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS `chatmembers` (
+  `chat` bigint NOT NULL,
+  `user` bigint NOT NULL,
+  `moderator` tinyint(1) NOT NULL,
+  PRIMARY KEY (`chat`,`user`),
+  KEY `chat` (`chat`)
+);
+
+CREATE TABLE IF NOT EXISTS `complaints` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `chat` bigint NOT NULL,
+  `message` int NOT NULL,
+  `complainant` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `chat` (`chat`,`message`) USING BTREE
+);
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `chat` bigint NOT NULL,
+  `message` int NOT NULL,
+  `user` bigint NOT NULL,
+  `isspam` int NOT NULL,
+  PRIMARY KEY (`chat`,`message`)
+);
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `rate` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
