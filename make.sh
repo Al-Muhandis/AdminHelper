1a49c92fd550b43230a49c5d631d4407b3a0fe5d
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-10-30 16:23:00 +0200

    fix make.sh

--- make.sh
+++ make.sh
@@ -9,25 +9,9 @@ Options:
 EOF
 )
 
-function priv_use
-(
-    [[ -d 'use' ]] || mkdir 'use'
-    for item in "${@}"; do
-        declare -A VAR=(
-            [url]="https://packages.lazarus-ide.org/${item}.zip"
-            [out]=$(mktemp)
-            [dir]=${item##/*}
-        )
-        wget --output-document "VAR[out]" "VAR[url]"
-        unzip -o "VAR[out]" -d "use/${VAR[dir]}"
-        rm --verbose "VAR[out]"
-    done
-)
-
 function pub_build
 (
     git submodule update --init --recursive
-    priv_use "1" "1" "1" "1" "1"
     find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} \;
     find 'src' -type 'f' -name '*.lpi' -exec lazbuild --recursive --build-mode=release {} \;
 )
