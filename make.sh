5c99ace0a6d31cedea79205ba8e3bec2e33e1b1b
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-10-30 16:10:39 +0200

    add github-actions

---
+++ make.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+function priv_clippit
+(
+    cat <<EOF
+Usage: bash ${0} [OPTIONS]
+Options:
+    build   Build program
+EOF
+)
+
+function priv_use
+(
+    [[ -d 'use' ]] || mkdir 'use'
+    for item in "${@}"; do
+        declare -A VAR=(
+            [url]="https://packages.lazarus-ide.org/${item}.zip"
+            [out]=$(mktemp)
+            [dir]=${item##/*}
+        )
+        wget --output-document "VAR[out]" "VAR[url]"
+        unzip -o "VAR[out]" -d "use/${VAR[dir]}"
+        rm --verbose "VAR[out]"
+    done
+)
+
+function pub_build
+(
+    git submodule update --init --recursive
+    priv_use "1" "1" "1" "1" "1"
+    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} \;
+    find 'src' -type 'f' -name '*.lpi' -exec lazbuild --recursive --build-mode=release {} \;
+)
+
+function priv_main
+(
+    set -euo pipefail
+    if ! (which lazbuild); then
+        source '/etc/os-release'
+        case ${ID:?} in
+            debian | ubuntu)
+                sudo apt-get update
+                sudo apt-get install -y lazarus
+            ;;
+        esac
+    fi
+    if ((${#})); then
+        case ${1} in
+            build) pub_build 1>&2 ;;
+        esac
+    else
+        priv_clippit
+    fi
+)
+
+priv_main "${@}" >/dev/null
